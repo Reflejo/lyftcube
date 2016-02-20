30ddfba0f8a71b7708a78480446a1f78dedee361
Martin Conte Mac Donell <martin@lyft.com>
2016-02-20 05:41:25 +0000

    Fix GPIOs after rpi bridge change

--- cube/GPIO.h
+++ cube/GPIO.h
@@ -6,14 +6,14 @@
 
 #define ENABLE          RPI_BPLUS_GPIO_J8_15
 
-#define LEVEL1          RPI_BPLUS_GPIO_J8_29
-#define LEVEL2          RPI_BPLUS_GPIO_J8_31
+#define LEVEL1          RPI_BPLUS_GPIO_J8_37
+#define LEVEL2          RPI_BPLUS_GPIO_J8_35
 #define LEVEL3          RPI_BPLUS_GPIO_J8_33
-#define LEVEL4          RPI_BPLUS_GPIO_J8_35
-#define LEVEL5          RPI_BPLUS_GPIO_J8_37
-#define LEVEL6          RPI_BPLUS_GPIO_J8_32
-#define LEVEL7          RPI_BPLUS_GPIO_J8_36
-#define LEVEL8          RPI_BPLUS_GPIO_J8_38
+#define LEVEL4          RPI_BPLUS_GPIO_J8_31
+#define LEVEL5          RPI_BPLUS_GPIO_J8_29
+#define LEVEL6          RPI_BPLUS_GPIO_J8_36
+#define LEVEL7          RPI_BPLUS_GPIO_J8_38
+#define LEVEL8          RPI_BPLUS_GPIO_J8_40
 
 /**
  * Turn off all LEDs and finalize SPI.
